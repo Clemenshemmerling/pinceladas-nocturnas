861427930b215685736d8b539528159b297a948b
Clemens Hemmerling <clemens@galileo.edu>
2016-09-11 19:58:58 -0600

    Creacion del admin

--- app/PinceladasNocturnas/settings.py
+++ app/PinceladasNocturnas/settings.py
@@ -31,7 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'galeria.app.GaleriaConfig',
+    'galeria.apps.GaleriaConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- app/galeria/models.py
+++ app/galeria/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Galeria(models.Model):
+	nombre = models.CharField(max_length=255)
+	imagen = models.ImageField(blank = True)
+	descripcion = models.CharField(max_length=255)
+	precio = models.DecimalField(max_digits=6, decimal_places=2)
+
+	def __str__(self):
+		return self.nombre
