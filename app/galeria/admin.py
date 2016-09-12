ea93e7da3c4266c8efbca52974e75712745d08eb
Clemens Hemmerling <clemens@galileo.edu>
2016-09-11 23:06:49 -0600

    Creamos url y las vistas

--- app/galeria/admin.py
+++ app/galeria/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
 from .models import Galeria
 # Register your models here.
-admin.site.register(Galeria)
+@admin.register(Galeria)
+class AdminGaleria(admin.ModelAdmin):
+	list_display = ('id', 'nombre', 'descripcion', 'precio',)
--- app/galeria/views.py
+++ app/galeria/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template import loader
+from .models import Galeria
 # Create your views here.
+
+def galeria(request):
+	galeria = Galeria.objects.order_by('id')
+	template = loader.get_template('base.html')
+	ctn = {
+		'galeria': galeria
+	}
+
+	return HttpResponse(template.render(ctn, request))
