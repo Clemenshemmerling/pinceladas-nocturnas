cda91692cd338c96cc298c924445b4b8cb2bd655
Clemens Hemmerling <clemens@galileo.edu>
2016-09-15 20:42:16 -0600

    Empezamos a agregar js al proyecto

---
+++ app/static/js/main.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $(document)[0].oncontextmenu = function() { return false; }
+
+    $(document).mousedown(function(e) {
+        if( e.button == 2 ) {
+            alert('Perdona tienes que comprar la imagen para poder descargarla');
+            return false;
+        } else {
+            return true;
+        }
+    });
+});
