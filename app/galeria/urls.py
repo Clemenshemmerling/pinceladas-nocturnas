e9551e34953e715886593d556b4dad52b018d52f
Clemens Hemmerling <clemens@galileo.edu>
2016-11-19 23:20:34 -0600

    Creacion de detalles galeria

--- app/galeria/urls.py
+++ app/galeria/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.galeria, name='galeria'),
+	url(r'^galeria/(?P<pk>[0-9]+)/$', views.galeria_detail, name='galeria_detail'),
 ]
--- app/galeria/views.py
+++ app/galeria/views.py
@@ -1,5 +1,6 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
+from django.shortcuts import render, get_object_or_404
 from .models import Galeria
 # Create your views here.
 
@@ -10,6 +11,17 @@ def galeria(request):
 	ctn = {
 		'galeria': galeria,
         'title': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def galeria_detail(request, pk):
+	galeria = get_object_or_404(Galeria, pk=pk)
+	template = loader.get_template('galeria_detail.html')
+	title = 'Galeria de fotos Pinceladas Nocturnas'
+	ctn = {
+		'galeria': galeria,
+		'title': title
 	}
 
 	return HttpResponse(template.render(ctn, request))
