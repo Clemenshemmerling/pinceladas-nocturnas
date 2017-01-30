60967a8934ffdef4b76eeeadfd93d777d2ee319d
Clemens Hemmerling <clemens@galileo.edu>
2017-01-29 21:37:39 -0600

    Creamos templates views y urls

--- app/galeria/urls.py
+++ app/galeria/urls.py
@@ -3,6 +3,17 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index, name='index'),
-	url(r'^galeria_fotos/', views.galeria, name='galeria'),
+	url(r'^galeria/', views.galeria, name='galeria'),
 	url(r'^galeria/(?P<pk>[0-9]+)/$', views.galeria_detail, name='galeria_detail'),
+	url(r'^contact/', views.contacto, name='contact'),
+	url(r'^planetario', views.planetario, name='planetario'),
+	url(r'^service', views.service, name='service'),
+	url(r'^about', views.about, name='about'),
+	url(r'^astronomy', views.astronomy, name='astronomy'),
+	url(r'^astrophotography', views.astrophotography, name='astrophotography'),
+	url(r'^prints', views.prints, name='prints'),
+	url(r'^deep_sky', views.deep_sky, name='deep_sky'),
+	url(r'^planetary', views.planetary, name='planetary'),
+	url(r'^wield_field', views.wield_field, name='wield_field'),
+	url(r'^landscape', views.landscape, name='landscape'),
 ]
--- app/galeria/views.py
+++ app/galeria/views.py
@@ -36,3 +36,111 @@ def galeria_detail(request, pk):
 	}
 
 	return HttpResponse(template.render(ctn, request))
+
+def contacto(request):
+	template = loader.get_template('contact.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def planetario(request):
+	template = loader.get_template('planetario.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def service(request):
+	template = loader.get_template('services.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def about(request):
+	template = loader.get_template('about.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def about(request):
+	template = loader.get_template('about.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def astronomy(request):
+	template = loader.get_template('astronomy.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def astrophotography(request):
+	template = loader.get_template('astrophotography.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def prints(request):
+	template = loader.get_template('prints.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def deep_sky(request):
+	template = loader.get_template('deep_sky.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def planetary(request):
+	template = loader.get_template('planetary.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def wield_field(request):
+	template = loader.get_template('wield_field.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
+
+def landscape(request):
+	template = loader.get_template('landscape.html')
+	title = 'Contact'
+	ctn = {
+		'title:': title
+	}
+
+	return HttpResponse(template.render(ctn, request))
